3eacec2ca39764f3c0a7b47ff2ad4baa3db530df
itzguled <itzguled55337@gmail.com>
2022-01-09 20:13:37 -0600

    added a function to generate questions

--- Develop/script.js
+++ Develop/script.js
@@ -13,5 +13,38 @@ function writePassword() {
 
 }
 
+function generatePassword() {
+var passwordLength = parseInt(prompt("How long will your password be? A length of at least 8 characters and no more than 128"));
+
+
+if (!passwordLength) {
+ alert("You need to provide a valid answer!");
+ generatePassword();
+} 
+else if (passwordLength < 8 || passwordLength > 128) {
+  alert("You need a password between 8 and 128 letters!");
+  generatePassword();
+} 
+else {
+  var confirmLowerCase = confirm("Do you want lowercase?");
+  var confirmUpperCase = confirm("Do you want uppercase?");
+  var confirmNumeric = confirm("Do you wany numbers?");
+  var confirmSpecial = confirm("Do you want any special characters?");
+}
+
+if (!confirmLowerCase && !confirmUpperCase && !confirmNumeric && !confirmSpecial)
+{
+alert("You need to pick atleast one option!");
+}
+
+
+
+};
+
+generatePassword()
+
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+

70e1fe067da34f5fb183a9c78c6ac14fb4962d52
itzguled <itzguled55337@gmail.com>
2022-01-16 13:29:14 -0600

    readme

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,4 @@
-//Initialize an empty array 
+//Initialize an empty array
 let finalOutput = [];
 
 // Get references to the #generate element
@@ -7,7 +7,7 @@ var generateBtn = document.querySelector("#generate");
 var passwordText = document.querySelector("#password");
 // Write password to the #password input
 function writePassword(finalString) {
-  passwordText.value = finalString; 
+  passwordText.value = finalString;
 }
 
 // function clearValue() {
@@ -59,46 +59,32 @@ function randomPassword(
   confirmNumeric,
   confirmSpecial
 ) {
-  // know password length
-  console.log("passwordLength", passwordLength);
-  //need to know if they say yes to lower case
-  //make variables that are lower case make character choice
-  // confirmLowerCase
   var chars = "";
 
-  console.log(confirmLowerCase);
   if (confirmLowerCase) {
     chars += "abcdefghijklmnopqrstuvwxyz";
   }
 
-  console.log(confirmUpperCase);
   if (confirmUpperCase) {
     chars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   }
 
-  console.log(confirmNumeric);
   if (confirmNumeric) {
     chars += "0123456789";
   }
 
-  console.log(confirmSpecial);
   if (confirmSpecial) {
     chars += "!@#$%^&*_-+=";
   }
 
-  console.log('line 85', chars);
-  console.log('line 86', chars.split('')); 
-
-  var arr = chars.split('');
-  console.log('line 89', arr.length);
+  var arr = chars.split("");
 
   for (var i = 0; i < passwordLength; i++) {
-
     var rnum = Math.floor(Math.random() * arr.length);
     finalOutput.push(arr[rnum]);
   }
-  var finalString = finalOutput.join('')
-  console.log('line 113', finalString);
+  var finalString = finalOutput.join("");
+
   writePassword(finalString);
 }
 // Add event listener to generate button
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Password Generator Starter Code
+# Quick Password Generator Starter Code
+
+## The Motivation
+
+My motivation for this project is to delve into making a site more interactive and getting more comfortable with Javascript. There was a lot of ways I could've approaced this and it has grown my skills in the language
+
+## Challenges
+
+The main issue I dealt with was understanding the many nuances with Javascript. I had to implement many different tools such as arrays and if statments to get this project to work appropriately
+
+## Screenshot
+![ScreenShot](/Develop/pass.PNG)
+
+## Link
+https://itzguled.github.io/quick-password-generator/
